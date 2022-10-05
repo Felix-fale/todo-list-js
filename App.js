8763b92057d4b03afb1826c5f6d41ccff3f2ba06
Felix-fale <falefelix23@gmail.com>
2022-10-05 23:06:25 +0000

    AJOUT DE TODO A TODO-LIST

---
+++ App.js
@@ -0,0 +1,37 @@
+// SELECTEURS
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// ECOUTEURS
+todoButton.addEventListener("click", addTodo);
+
+// FONCTIONS
+function addTodo(e) {
+  e.preventDefault();
+  // console.log("hello");
+
+  //   ============= Creer une Div avec une class todo ==============
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //   ============ creer un li avec une class todo-item ===========
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "hey";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+
+  //   ===========creeer le bouton check =========
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("completed-btn");
+  todoDiv.appendChild(completedButton);
+
+  //   ===========creeer le bouton supprimer =========
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+  
+  //   AJOUTER NOTRE TODO A TODO-LIST
+  todoList.appendChild(todoDiv);
+}
